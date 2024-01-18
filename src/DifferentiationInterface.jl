f784a35de45eecc8f8243c96874dce58e81252ae
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-01-18 13:57:17 +0100

    First try with ForwardDiff and Enzyme (#4)

--- src/DifferentiationInterface.jl
+++ src/DifferentiationInterface.jl
@@ -1,5 +1,65 @@
+"""
+    DifferentiationInterface
+
+An experimental redesign for [AbstractDifferentiation.jl]
+(https://github.com/JuliaDiff/AbstractDifferentiation.jl).
+
+# Exports
+
+$(EXPORTS)
+"""
 module DifferentiationInterface
 
-# Write your package code here.
+using DocStringExtensions
+
+abstract type AbstractBackend end
+
+"""
+    ChainRulesBackend
+"""
+struct ChainRulesBackend <: AbstractBackend end
+
+"""
+    EnzymeBackend
+"""
+struct EnzymeBackend <: AbstractBackend end
+
+"""
+    ForwardDiffBackend
+"""
+struct ForwardDiffBackend <: AbstractBackend end
+
+"""
+    jvp!(dy, backend, f, x, dx)
+
+Compute a Jacobian-vector product and return `dy`.
+
+# Arguments
+
+- `dy`: cotangent, might be overwritten
+- `backend::AbstractBackend`: autodiff backend
+- `f`: function `x -> y` to differentiate
+- `x`: argument
+- `dx`: tangent
+"""
+function jvp! end
+
+"""
+    jvp!(dx, backend, f, x, dy)
+
+Compute a Jacobian-vector product and return `dx`.
+
+# Arguments
+
+- `dx`: tangent, might be overwritten
+- `backend::AbstractBackend`: autodiff backend
+- `f`: function `x -> y` to differentiate
+- `x`: argument
+- `dy`: cotangent
+"""
+function vjp! end
+
+export jvp!, vjp!
+export ChainRulesBackend, EnzymeBackend, ForwardDiffBackend
 
 end
