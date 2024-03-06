d0c48fbdd74f2fa5fcfb1cf10411d8fd27a47781
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-03-06 14:05:26 +0100

    Tests passing

--- test/enzyme.jl
+++ test/enzyme.jl
@@ -1,14 +1,24 @@
 using DifferentiationInterface
 using Enzyme
 
+# see https://github.com/EnzymeAD/Enzyme.jl/issues/1330
+
 @testset "EnzymeForwardBackend" begin
-    test_pushforward(EnzymeForwardBackend(); type_stability=true)
-    test_jacobian_and_friends(EnzymeForwardBackend(); type_stability=true)
-end
+    test_pushforward(
+        EnzymeForwardBackend();
+        output_type=Union{Number,AbstractVector},  # TODO: remove
+        type_stability=true,
+    )
+    test_jacobian_and_friends(
+        EnzymeForwardBackend();
+        output_type=Union{Number,AbstractVector},  # TODO: remove
+        type_stability=true,
+    )
+end;
 
 @testset "EnzymeReverseBackend" begin
     test_pullback(EnzymeReverseBackend(); output_type=Number, type_stability=true)
     test_jacobian_and_friends(
         EnzymeReverseBackend(); output_type=Number, type_stability=true
     )
-end
+end;
