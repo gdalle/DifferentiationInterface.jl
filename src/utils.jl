6d7824e76b8d1506686d062f8be7718dbd07fee4
Adrian Hill <adrian.hill@mailbox.org>
2024-03-06 15:08:58 +0100

    More docstring fixes

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,4 @@
 """
-    basisarray(a::AbstractArray, i)
     basisarray(backend, a::AbstractArray, i)
 
 Construct the `i`-th stardard basis array in the vector space of `a` with element type `eltype(a)`.
@@ -8,10 +7,6 @@ Construct the `i`-th stardard basis array in the vector space of `a` with elemen
 
 If an AD backend benefits from a more specialized unit vector implementation,
 this function can be extended on the backend type.
-
-function basisarray(::AbstractBackend, a::AbstractVector{T}, i::Integer) where {T}
-    return OneElement(one(T), i, length(v))
-end
 """
 basisarray(::AbstractBackend, a::AbstractArray, i) = basisarray(a, i)
 
