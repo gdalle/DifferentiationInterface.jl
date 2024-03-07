ae26e9661c2b311a57ba6d9881d9e247d8ff0cab
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-03-07 15:05:35 +0100

    Reactivate JET test

--- test/polyesterforwarddiff.jl
+++ test/polyesterforwarddiff.jl
@@ -7,5 +7,5 @@ test_jacobian_and_friends(
     PolyesterForwardDiffBackend(4; custom=true);
     input_type=Union{Number,AbstractVector},
     output_type=Union{Number,AbstractVector},
-    type_stability=false,
+    type_stability=true,
 );
