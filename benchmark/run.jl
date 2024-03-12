1bc5e9c34b96bfba54c0a721e1b2ec06b89c56c1
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-03-12 11:30:29 +0100

    Better dataframe

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -1,12 +1,12 @@
 using BenchmarkTools
 
 include("benchmarks.jl")
-include("dataframe.jl")
 
 # Run benchmarks locally
 results = BenchmarkTools.run(SUITE; verbose=true)
 
 # Parse into dataframe
+include("dataframe.jl")
 data = parse_benchmark_results(results; path=joinpath(@__DIR__, "results.csv"))
 
 # Compare commits locally
