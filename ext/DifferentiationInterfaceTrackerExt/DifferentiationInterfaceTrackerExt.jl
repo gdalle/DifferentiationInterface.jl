23dd2ca8f87156ec8b42bfee855da0dc7c5da49a
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-03-19 08:40:51 +0100

    No prefix

--- ext/DifferentiationInterfaceTrackerExt/DifferentiationInterfaceTrackerExt.jl
+++ ext/DifferentiationInterfaceTrackerExt/DifferentiationInterfaceTrackerExt.jl
@@ -3,13 +3,13 @@ module DifferentiationInterfaceTrackerExt
 using ADTypes: AutoTracker
 import DifferentiationInterface as DI
 using DifferentiationInterface: update!
-using Tracker: Tracker, back, forward, gradient, jacobian, param, withgradient
+using Tracker: Tracker, back, data, forward, gradient, jacobian, param, withgradient
 
 ## Pullback
 
 function DI.value_and_pullback(::AutoTracker, f, x, dy, extras::Nothing)
     y, back = forward(f, x)
-    return y, Tracker.data(only(back(dy)))
+    return y, data(only(back(dy)))
 end
 
 function DI.value_and_pullback!(dx, backend::AutoTracker, f, x, dy, extras::Nothing)
@@ -32,7 +32,7 @@ function DI.value_and_gradient!(
     grad::AbstractArray, backend::AutoTracker, f, x::AbstractArray, extras::Nothing
 )
     y, new_grad = DI.value_and_gradient(backend, f, x, extras)
-    grad .= Tracker.data(new_grad)
+    grad .= data(new_grad)
     return y, grad
 end
 
@@ -40,7 +40,7 @@ function DI.gradient!(
     grad::AbstractArray, backend::AutoTracker, f, x::AbstractArray, extras::Nothing
 )
     new_grad = DI.gradient(backend, f, x, extras)
-    grad .= Tracker.data(new_grad)
+    grad .= data(new_grad)
     return grad
 end
 
