4c0912f02ea6335ce39b2c23d701fc8ec85bffd9
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-03-24 18:47:04 +0100

    Fix nested norm

--- src/DifferentiationTest/scenarios_nested.jl
+++ src/DifferentiationTest/scenarios_nested.jl
@@ -1,6 +1,5 @@
 nested_norm(x::Number) = abs2(x)
-nested_norm(x::AbstractArray) = sum(nested_norm, x)
-nested_norm(x::Tuple) = sum(nested_norm, x)
+nested_norm(x) = sum(nested_norm, x)
 
 function nested_scenarios()
     scenarios = [Scenario(nested_norm; x=([1.0, 2.0], [3.0, 4.0, 5.0]))]
