d0adf621eb68b1fbba2fac7363457faf7e8de709
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-03-26 10:17:59 +0100

    Enzyme split reverse mode (#102)

--- test/enzyme_reverse.jl
+++ test/enzyme_reverse.jl
@@ -5,15 +5,4 @@ using Enzyme: Enzyme
 @test check_available(AutoEnzyme(Enzyme.Reverse))
 @test check_mutation(AutoEnzyme(Enzyme.Reverse))
 
-test_differentiation(
-    AutoEnzyme(Enzyme.Reverse);
-    output_type=Number,
-    type_stability=true,
-    allocating=true,
-    mutating=false,
-    second_order=false,
-);
-
-test_differentiation(
-    AutoEnzyme(Enzyme.Reverse); mutating=true, allocating=false, second_order=false
-);
+test_differentiation(AutoEnzyme(Enzyme.Reverse); second_order=false);
