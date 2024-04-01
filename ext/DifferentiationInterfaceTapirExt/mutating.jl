2151413882d6f3e4f7d6faa268fdf61c74534a38
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-04-01 17:07:03 +0200

    Fix

--- ext/DifferentiationInterfaceTapirExt/mutating.jl
+++ ext/DifferentiationInterfaceTapirExt/mutating.jl
@@ -2,12 +2,9 @@ function DI.value_and_pullback!!(f!, y, dx, ::AutoTapir, x, dy, extras::Nothing)
     rrule = build_rrule(f!, y, x)
     dy_righttype = convert(typeof(y), dy)
     dx_righttype = zero_sametype!!(dx, x)
+    dz = nothing  # f!(y, x) = nothing
     _, (_, _, new_dx) = value_and_pullback!!(
-        rrule,
-        dy_righttype,
-        zero_codual(f!),
-        CoDual(y, dy_righttype),
-        CoDual(x, dx_righttype),
+        rrule, dz, zero_codual(f!), CoDual(y, dy_righttype), CoDual(x, dx_righttype)
     )
     return y, new_dx
 end
