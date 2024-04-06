b88d44ac879ede5f9b38c5223ef167df7bf0fd2c
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-04-06 11:51:06 +0200

    Call Diffractor's native pushforward

--- DifferentiationInterface/ext/DifferentiationInterfaceDiffractorExt/DifferentiationInterfaceDiffractorExt.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceDiffractorExt/DifferentiationInterfaceDiffractorExt.jl
@@ -1,10 +1,9 @@
 module DifferentiationInterfaceDiffractorExt
 
-import AbstractDifferentiation as AD  # public API for Diffractor
 using ADTypes: ADTypes, AutoChainRules, AutoDiffractor
 import DifferentiationInterface as DI
 using DifferentiationInterface: NoPushforwardExtras
-using Diffractor: DiffractorForwardBackend, DiffractorRuleConfig
+using Diffractor: DiffractorRuleConfig, TaylorTangentIndex, ZeroBundle, bundle, ∂☆
 
 DI.supports_mutation(::AutoDiffractor) = DI.MutationNotSupported()
 DI.mode(::AutoDiffractor) = ADTypes.AbstractForwardMode
@@ -14,10 +13,17 @@ DI.mode(::AutoChainRules{<:DiffractorRuleConfig}) = ADTypes.AbstractForwardMode
 
 DI.prepare_pushforward(f, ::AutoDiffractor, x) = NoPushforwardExtras()
 
-function DI.value_and_pushforward(f, ::AutoDiffractor, x, dx, ::NoPushforwardExtras)
-    vpff = AD.value_and_pushforward_function(DiffractorForwardBackend(), f, x)
-    y, dy = vpff((dx,))
-    return y, dy
+function DI.pushforward(f, ::AutoDiffractor, x, dx, ::NoPushforwardExtras)
+    # code copied from Diffractor.jl
+    z = ∂☆{1}()(ZeroBundle{1}(f), bundle(x, dx))
+    dy = z[TaylorTangentIndex(1)]
+    return dy
+end
+
+function DI.value_and_pushforward(
+    f, backend::AutoDiffractor, x, dx, extras::NoPushforwardExtras
+)
+    return f(x), DI.pushforward(f, backend, x, dx, extras)
 end
 
 end
