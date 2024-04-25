207b9b76ccabcb6f2aaf4f808b07ac4328f7a21d
adrhill <adrian.hill@mailbox.org>
2024-04-25 16:45:37 +0200

    Test exceptions

---
+++ DifferentiationInterface/test/test_exceptions.jl
@@ -0,0 +1,33 @@
+using DifferentiationInterface: MissingBackendError
+
+"""
+    AutoBrokenForward <: ADTypes.AbstractADType
+
+Available forward-mode backend with no pushforward implementation.
+Used to test error messages.
+"""
+struct AutoBrokenForward <: AbstractADType end
+ADTypes.mode(::AutoBrokenForward) = ADTypes.ForwardMode()
+DifferentiationInterface.check_available(::AutoBrokenForward) = true
+
+"""
+    AutoBrokenReverse <: ADTypes.AbstractADType
+
+Available reverse-mode backend with no pullback implementation.
+Used to test error messages. 
+"""
+struct AutoBrokenReverse <: AbstractADType end
+ADTypes.mode(::AutoBrokenReverse) = ADTypes.ReverseMode()
+DifferentiationInterface.check_available(::AutoBrokenReverse) = true
+
+## Test exceptions
+@testset "MissingBackendError" begin
+    f(x::AbstractArray) = sum(abs2, x)
+    x = [1.0, 2.0, 3.0]
+
+    @test_throws MissingBackendError gradient(f, AutoBrokenForward(), x)
+    @test_throws MissingBackendError gradient(f, AutoBrokenReverse(), x)
+
+    @test_throws MissingBackendError hvp(f, AutoBrokenForward(), x, x)
+    @test_throws MissingBackendError hvp(f, AutoBrokenReverse(), x, x)
+end
