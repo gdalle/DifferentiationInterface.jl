07709ea83a5e20932f93ee6daf54b6b2d7abcd73
adrhill <adrian.hill@mailbox.org>
2024-04-25 18:13:32 +0200

    Run JuliaFormatter

--- DifferentiationInterface/src/utils/exceptions.jl
+++ DifferentiationInterface/src/utils/exceptions.jl
@@ -4,11 +4,17 @@ end
 function Base.showerror(io::IO, e::MissingBackendError)
     println(io, "failed to use $(backend_string(e.backend)) backend.")
     if !check_available(e.backend)
-        print(io, """Backend package is not loaded. To fix, run
+        print(
+            io,
+            """Backend package is not loaded. To fix, run
 
-          using $(backend_package_name(e.backend))
-        """)
+              using $(backend_package_name(e.backend))
+            """,
+        )
     else
-        print(io, "Please open an issue: https://github.com/gdalle/DifferentiationInterface.jl/issues/new")
+        print(
+            io,
+            "Please open an issue: https://github.com/gdalle/DifferentiationInterface.jl/issues/new",
+        )
     end
 end
