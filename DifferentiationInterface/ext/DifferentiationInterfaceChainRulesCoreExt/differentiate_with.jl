a3d481b62bf2ae868dc9640a8428b6a49bc7e467
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-13 18:49:29 +0200

    Fix DifferentiateWith

--- DifferentiationInterface/ext/DifferentiationInterfaceChainRulesCoreExt/differentiate_with.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceChainRulesCoreExt/differentiate_with.jl
@@ -1,3 +1,24 @@
+struct OneArgPullbackFunc{B,F,X,E}
+    f::F
+    backend::B
+    x::X
+    extras::E
+end
+
+function (pbf::OneArgPullbackFunc)(dy)
+    @compat (; f, backend, x, extras) = pbf
+    return pullback(f, backend, x, dy, extras)
+end
+
+function value_and_pullback_split(
+    f::F,
+    backend::AbstractADType,
+    x,
+    extras::PullbackExtras=prepare_pullback_same_point(f, backend, x, f(x)),
+) where {F}
+    return f(x), OneArgPullbackFunc(f, backend, x, extras)
+end
+
 function ChainRulesCore.frule((_, dx), dw::DifferentiateWith, x)
     @compat (; f, backend) = dw
     y, dy = DI.value_and_pushforward(f, backend, x, dx)
