9871a258c7c5572d755906028fe3126fc1dea7f4
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-13 19:12:30 +0200

    Imports

--- DifferentiationInterface/ext/DifferentiationInterfaceChainRulesCoreExt/differentiate_with.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceChainRulesCoreExt/differentiate_with.jl
@@ -7,7 +7,7 @@ end
 function ChainRulesCore.rrule(dw::DifferentiateWith, x)
     @compat (; f, backend) = dw
     y = f(x)
-    extras_same = prepare_pullback_same_point(f, backend, x, y)
-    pullbackfunc(dy) = (NoTangent(), pullback(f, backend, x, dy, extras_same))
+    extras_same = DI.prepare_pullback_same_point(f, backend, x, y)
+    pullbackfunc(dy) = (NoTangent(), DI.pullback(f, backend, x, dy, extras_same))
     return y, pullbackfunc
 end
