62f18b01ebe7ea5652e2d1ab2d1e6b89a64796b4
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-13 18:56:29 +0200

    Fix DifferentiateWith again

--- DifferentiationInterface/ext/DifferentiationInterfaceChainRulesCoreExt/differentiate_with.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceChainRulesCoreExt/differentiate_with.jl
@@ -1,24 +1,3 @@
-struct OneArgPullbackFunc{B,F,X,E}
-    f::F
-    backend::B
-    x::X
-    extras::E
-end
-
-function (pbf::OneArgPullbackFunc)(dy)
-    @compat (; f, backend, x, extras) = pbf
-    return pullback(f, backend, x, dy, extras)
-end
-
-function value_and_pullback_split(
-    f::F,
-    backend::AbstractADType,
-    x,
-    extras::PullbackExtras=prepare_pullback_same_point(f, backend, x, f(x)),
-) where {F}
-    return f(x), OneArgPullbackFunc(f, backend, x, extras)
-end
-
 function ChainRulesCore.frule((_, dx), dw::DifferentiateWith, x)
     @compat (; f, backend) = dw
     y, dy = DI.value_and_pushforward(f, backend, x, dx)
@@ -27,7 +6,8 @@ end
 
 function ChainRulesCore.rrule(dw::DifferentiateWith, x)
     @compat (; f, backend) = dw
-    y, pullbackfunc = DI.value_and_pullback_split(f, backend, x)
-    pullbackfunc_adjusted(dy) = (NoTangent(), pullbackfunc(dy))
-    return y, pullbackfunc_adjusted
+    y = f(x)
+    extras_same = prepare_pullback_same_point(f, backend, x, y)
+    pullbackfunc(dy) = (NoTangent(), pullback(f, backend, x, dy, extras_same))
+    return y, pullbackfunc
 end
