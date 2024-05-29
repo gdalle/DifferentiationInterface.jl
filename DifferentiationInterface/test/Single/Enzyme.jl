ebb8beab20a1a2d29236ce1b95770af82439a366
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-29 07:42:56 +0200

    Re-add Enzyme test (#279)
    * Re-add Enzyme test
    
    * Typo

--- DifferentiationInterface/test/Single/Enzyme.jl
+++ DifferentiationInterface/test/Single/Enzyme.jl
@@ -31,8 +31,11 @@ end
 
 ## Dense backends
 
+test_differentiation(dense_backends; second_order=false, logging=LOGGING);
+
 test_differentiation(
     AutoEnzyme(; mode=Enzyme.Forward);  # TODO: add more
+    correctness=false,
     type_stability=true,
     second_order=false,
     logging=LOGGING,
