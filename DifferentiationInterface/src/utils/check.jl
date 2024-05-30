eb20729887f8db2d50ec42fc88383266281b49d3
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-30 06:52:01 +0200

    Correct hess_th

--- DifferentiationInterface/src/utils/check.jl
+++ DifferentiationInterface/src/utils/check.jl
@@ -28,11 +28,11 @@ Check whether `backend` supports second order differentiation by trying to compu
 """
 function check_hessian(backend::AbstractADType; verbose=true)
     try
-        x = [1.0, 3.0]
+        x = [2.0, 3.0]
         hess = hessian(hess_checker, backend, x)
         hess_th = [
-            2*x[2] 4*x[1]*x[2]
-            4*x[1]*x[2] 2*x[1]
+            2*abs2(x[2]) 4*x[1]*x[2]
+            4*x[1]*x[2] 2*abs2(x[1])
         ]
         return isapprox(hess, hess_th; rtol=1e-3)
     catch exception
