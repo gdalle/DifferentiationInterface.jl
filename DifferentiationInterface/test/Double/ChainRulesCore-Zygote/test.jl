beb098c60089291f5836e9f01392cb29c6c701aa
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-05 07:59:27 +0200

    Split out ChainRulesCore

---
+++ DifferentiationInterface/test/Double/ChainRulesCore-Zygote/test.jl
@@ -0,0 +1,17 @@
+using ChainRulesCore
+using DifferentiationInterface, DifferentiationInterfaceTest
+using Test
+using Zygote: ZygoteRuleConfig
+
+for backend in AutoChainRules(ZygoteRuleConfig())
+    @test check_available(backend)
+    @test !check_twoarg(backend)
+    @test check_hessian(backend)
+end
+
+test_differentiation(
+    AutoChainRules(ZygoteRuleConfig());
+    excluded=[SecondDerivativeScenario],
+    second_order=VERSION >= v"1.10",
+    logging=LOGGING,
+);
