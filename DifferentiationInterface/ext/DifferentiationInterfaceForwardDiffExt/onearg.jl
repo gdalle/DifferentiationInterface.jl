153870899a9c47be112acd4e32518bcfde340ba2
Gabriel Gerlero <gerlero@users.noreply.github.com>
2024-06-07 09:18:44 -0300

    Add derivative! and value_and_derivative! methods

--- DifferentiationInterface/ext/DifferentiationInterfaceForwardDiffExt/onearg.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceForwardDiffExt/onearg.jl
@@ -71,12 +71,25 @@ function DI.derivative(f::F, ::AutoForwardDiff, x, ::NoDerivativeExtras) where {
     return derivative(f, x)
 end
 
+function DI.derivative!(f::F, der, ::AutoForwardDiff, x, ::NoDerivativeExtras) where {F}
+    return derivative!(der, f, x)
+end
+
 function DI.value_and_derivative(
     f::F, backend::AutoForwardDiff, x, ::NoDerivativeExtras
 ) where {F}
     T = tag_type(f, backend, x)
     ydual = f(make_dual(T, x, one(x)))
     return myvalue(T, ydual), myderivative(T, ydual)
+end
+
+function DI.value_and_derivative!(
+    f::F, der, ::AutoForwardDiff, x, ::NoDerivativeExtras
+) where {F}
+    T = tag_type(f, backend, x)
+    xdual = make_dual(T, x, one(x))
+    ydual = f(xdual)
+    return myvalue(T, ydual), myderivative!(T, der, ydual)
 end
 
 function DI.value_derivative_and_second_derivative(
