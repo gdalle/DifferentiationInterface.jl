86f1e02370fab04bdfd6da4db9e484e48cb57e8c
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-25 10:07:22 +0200

    Batched ForwardDiff pushforward (#328)
    * Batched ForwardDiff pushforward
    
    * Ambiguity
    
    * Typo
    
    * Add batched mode to AutoFromPrimitive
    
    * Remove type test
    
    * Fix type instability

--- DifferentiationInterface/ext/DifferentiationInterfaceForwardDiffExt/utils.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceForwardDiffExt/utils.jl
@@ -1,23 +1,47 @@
 choose_chunk(::AutoForwardDiff{nothing}, x) = Chunk(x)
-choose_chunk(::AutoForwardDiff{C}, x) where {C} = Chunk{C}()
+choose_chunk(::AutoForwardDiff{C}, x) where {C} = Chunk{min(length(x), C)}()
 
 tag_type(f, ::AutoForwardDiff{C,T}, x) where {C,T} = T
 tag_type(f, ::AutoForwardDiff{C,Nothing}, x) where {C} = Tag{typeof(f),eltype(x)}
 
-make_dual_similar(::Type{T}, x::Number) where {T} = Dual{T}(x, x)
-make_dual_similar(::Type{T}, x) where {T} = similar(x, Dual{T,eltype(x),1})
+make_dual_similar(::Type{T}, x::Number, dx::Number) where {T} = Dual{T}(x, dx)
+make_dual_similar(::Type{T}, x, dx) where {T} = similar(x, Dual{T,eltype(x),1})
 
-make_dual(::Type{T}, x::Number, dx) where {T} = Dual{T}(x, dx)
+function make_dual_similar(::Type{T}, x, dx::Batch{B}) where {T,B}
+    return similar(x, Dual{T,eltype(x),B})
+end
+
+make_dual(::Type{T}, x::Number, dx::Number) where {T} = Dual{T}(x, dx)
 make_dual!(::Type{T}, xdual, x, dx) where {T} = map!(Dual{T}, xdual, x, dx)
 
-myvalue(::Type{T}, ydual::Number) where {T} = value(T, ydual)
-myvalue(::Type{T}, ydual) where {T} = map(Fix1(value, T), ydual)
+function make_dual!(::Type{T}, xdual, x, dx::Batch{B}) where {T,B}
+    return map!(Dual{T}, xdual, x, dx.elements...)
+end
 
-myvalue!(::Type{T}, y, ydual) where {T} = map!(Fix1(value, T), y, ydual)
+myvalue(::Type{T}, ydual::Dual{T}) where {T} = value(T, ydual)
+myvalue(::Type{T}, ydual) where {T} = map(Fix1(myvalue, T), ydual)
 
-myderivative(::Type{T}, ydual::Number) where {T} = extract_derivative(T, ydual)
-myderivative(::Type{T}, ydual) where {T} = map(Fix1(extract_derivative, T), ydual)
+function myvalue!(::Type{T}, y, ydual) where {T}
+    return map!(Fix1(myvalue, T), y, ydual)
+end
+
+myderivative(::Type{T}, ydual::Dual{T}) where {T} = extract_derivative(T, ydual)
+myderivative(::Type{T}, ydual) where {T} = map(Fix1(myderivative, T), ydual)
 
 function myderivative!(::Type{T}, dy, ydual) where {T}
-    return map!(Fix1(extract_derivative, T), dy, ydual)
+    return map!(Fix1(myderivative, T), dy, ydual)
+end
+
+function mypartials(::Type{T}, ::Val{B}, ydual) where {T,B}
+    elements = ntuple(Val(B)) do b
+        partials.(T, ydual, b)
+    end
+    return Batch(elements)
+end
+
+function mypartials!(::Type{T}, dy::Batch{B}, ydual) where {T,B}
+    for b in eachindex(dy.elements)
+        dy.elements[b] .= partials.(T, ydual, b)
+    end
+    return dy
 end
--- DifferentiationInterface/src/misc/from_primitive.jl
+++ DifferentiationInterface/src/misc/from_primitive.jl
@@ -11,6 +11,8 @@ end
 
 ADTypes.mode(::AutoForwardFromPrimitive) = ADTypes.ForwardMode()
 
+### Standard
+
 function prepare_pushforward(f, fromprim::AutoForwardFromPrimitive, x, dx)
     return prepare_pushforward(f, fromprim.backend, x, dx)
 end
@@ -43,6 +45,55 @@ function value_and_pushforward!(
     return value_and_pushforward!(f!, y, dy, fromprim.backend, x, dx, extras)
 end
 
+### Batched
+
+function prepare_pushforward_batched(
+    f, fromprim::AutoForwardFromPrimitive, x, dx::Batch{B}
+) where {B}
+    return prepare_pushforward_batched(f, fromprim.backend, x, dx)
+end
+
+function prepare_pushforward_batched(
+    f!, y, fromprim::AutoForwardFromPrimitive, x, dx::Batch{B}
+) where {B}
+    return prepare_pushforward_batched(f!, y, fromprim.backend, x, dx)
+end
+
+function pushforward_batched(
+    f, fromprim::AutoForwardFromPrimitive, x, dx::Batch{B}, extras::PushforwardExtras
+) where {B}
+    return pushforward_batched(f, fromprim.backend, x, dx, extras)
+end
+
+function pushforward_batched(
+    f!, y, fromprim::AutoForwardFromPrimitive, x, dx::Batch{B}, extras::PushforwardExtras
+) where {B}
+    return pushforward_batched(f!, y, fromprim.backend, x, dx, extras)
+end
+
+function pushforward_batched!(
+    f,
+    dy::Batch{B},
+    fromprim::AutoForwardFromPrimitive,
+    x,
+    dx::Batch{B},
+    extras::PushforwardExtras,
+) where {B}
+    return pushforward_batched!(f, dy, fromprim.backend, x, dx, extras)
+end
+
+function pushforward_batched!(
+    f!,
+    y,
+    dy::Batch{B},
+    fromprim::AutoForwardFromPrimitive,
+    x,
+    dx::Batch{B},
+    extras::PushforwardExtras,
+) where {B}
+    return pushforward_batched!(f!, y, dy, fromprim.backend, x, dx, extras)
+end
+
 ## Reverse
 
 struct AutoReverseFromPrimitive{B} <: FromPrimitive
@@ -50,6 +101,8 @@ struct AutoReverseFromPrimitive{B} <: FromPrimitive
 end
 
 ADTypes.mode(::AutoReverseFromPrimitive) = ADTypes.ReverseMode()
+
+### Standard
 
 function prepare_pullback(f, fromprim::AutoReverseFromPrimitive, x, dy)
     return prepare_pullback(f, fromprim.backend, x, dy)
@@ -81,4 +134,53 @@ function value_and_pullback!(
     f!, y, dx, fromprim::AutoReverseFromPrimitive, x, dy, extras::PullbackExtras
 )
     return value_and_pullback!(f!, y, dx, fromprim.backend, x, dy, extras)
+end
+
+### Batched
+
+function prepare_pullback_batched(
+    f, fromprim::AutoReverseFromPrimitive, x, dy::Batch{B}
+) where {B}
+    return prepare_pullback_batched(f, fromprim.backend, x, dy)
+end
+
+function prepare_pullback_batched(
+    f!, y, fromprim::AutoReverseFromPrimitive, x, dy::Batch{B}
+) where {B}
+    return prepare_pullback_batched(f!, y, fromprim.backend, x, dy)
+end
+
+function pullback_batched(
+    f, fromprim::AutoReverseFromPrimitive, x, dy::Batch{B}, extras::PullbackExtras
+) where {B}
+    return pullback_batched(f, fromprim.backend, x, dy, extras)
+end
+
+function pullback_batched(
+    f!, y, fromprim::AutoReverseFromPrimitive, x, dy::Batch{B}, extras::PullbackExtras
+) where {B}
+    return pullback_batched(f!, y, fromprim.backend, x, dy, extras)
+end
+
+function pullback_batched!(
+    f,
+    dx::Batch{B},
+    fromprim::AutoReverseFromPrimitive,
+    x,
+    dy::Batch{B},
+    extras::PullbackExtras,
+) where {B}
+    return pullback_batched!(f, dx, fromprim.backend, x, dy, extras)
+end
+
+function pullback_batched!(
+    f!,
+    y,
+    dx::Batch{B},
+    fromprim::AutoReverseFromPrimitive,
+    x,
+    dy::Batch{B},
+    extras::PullbackExtras,
+) where {B}
+    return pullback_batched!(f!, y, dx, fromprim.backend, x, dy, extras)
 end
