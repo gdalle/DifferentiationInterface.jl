a7dbb0e9ba20a065ba2da71338a02788d8e554d5
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-26 14:34:01 +0200

    Fix DenseSparsityDetector

--- DifferentiationInterface/src/misc/sparsity_detector.jl
+++ DifferentiationInterface/src/misc/sparsity_detector.jl
@@ -73,7 +73,10 @@ end
 
 function Base.show(io::IO, detector::DenseSparsityDetector{method}) where {method}
     @compat (; backend, atol) = detector
-    return print(io, "DenseSparsityDetector{:$method}($backend; atol=$atol)")
+    return print(
+        io,
+        "$(repr(DenseSparsityDetector; context=io))($(repr(backend; context=io)); atol=$atol, method=$(repr(method; context=io)))",
+    )
 end
 
 function DenseSparsityDetector(
