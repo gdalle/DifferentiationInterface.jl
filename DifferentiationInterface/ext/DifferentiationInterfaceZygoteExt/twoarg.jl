1e1031f2992c4f669568cd9b8b5fa022ba4b2a77
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-07-16 09:39:13 +0200

    Use Zygote Buffer for f!

---
+++ DifferentiationInterface/ext/DifferentiationInterfaceZygoteExt/twoarg.jl
@@ -0,0 +1,22 @@
+struct Bufferize{F,Y}
+    f!::F
+    y::Y
+end
+
+function (f::Bufferize)(x)
+    @compat (; f!, y) = f
+    buf = Buffer(y)
+    f!(buf, x)
+    return copy(buf)
+end
+
+## Pullback
+
+DI.prepare_pullback(f!, y, ::AutoZygote, x, dy) = NoPullbackExtras()
+
+function DI.value_and_pullback(f!, y, backend::AutoZygote, x, dy, ::NoPullbackExtras)
+    f = Bufferize(f!, y)
+    y_new, dx = DI.value_and_pullback(f, backend, x, dy)
+    copyto!(y_new, y)
+    return y, dx
+end
