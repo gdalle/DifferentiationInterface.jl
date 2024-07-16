989cbe9202ebf854eadc5868be8f7ab096e9e9bf
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-07-16 11:49:37 +0200

    Put test dependencies in individual projects

---
+++ DifferentiationInterface/test/Back/SecondOrder/test.jl
@@ -0,0 +1,49 @@
+using DifferentiationInterface, DifferentiationInterfaceTest
+import DifferentiationInterface as DI
+using Enzyme: Enzyme
+using ForwardDiff: ForwardDiff
+using ReverseDiff: ReverseDiff
+using SparseConnectivityTracer
+using SparseMatrixColorings
+using Zygote: Zygote
+using Test
+
+## Dense
+
+onearg_backends = [
+    SecondOrder(AutoForwardDiff(), AutoZygote()),
+    SecondOrder(AutoReverseDiff(), AutoZygote()),
+]
+
+twoarg_backends = [
+    SecondOrder(AutoForwardDiff(), AutoEnzyme(; mode=Enzyme.Forward)),
+    SecondOrder(AutoEnzyme(; mode=Enzyme.Reverse), AutoForwardDiff()),
+]
+
+for backend in vcat(onearg_backends, twoarg_backends)
+    @test check_available(backend)
+    if backend in onearg_backends
+        @test !check_twoarg(backend)
+    else
+        @test check_twoarg(backend)
+    end
+    @test check_hessian(backend)
+end
+
+test_differentiation(
+    vcat(onearg_backends, twoarg_backends); first_order=false, logging=LOGGING
+);
+
+## Sparse
+
+sparse_backends = [
+    AutoSparse(
+        SecondOrder(AutoForwardDiff(), AutoZygote());
+        sparsity_detector=TracerSparsityDetector(),
+        coloring_algorithm=GreedyColoringAlgorithm(),
+    ),
+]
+
+test_differentiation(
+    sparse_backends, sparse_scenarios(); first_order=false, sparsity=true, logging=LOGGING
+);
