821e6dfaa874dbdf2ae7e77143ee53962fb182e4
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-07-19 09:51:34 +0200

    Add Tracker tests

--- DifferentiationInterface/test/Down/Flux/test.jl
+++ DifferentiationInterface/test/Down/Flux/test.jl
@@ -1,16 +1,19 @@
 using DifferentiationInterface, DifferentiationInterfaceTest
 import DifferentiationInterfaceTest as DIT
+using Enzyme: Enzyme
 using FiniteDifferences: FiniteDifferences
 using Flux: Flux
-using Enzyme: Enzyme
+using Random
+using Tracker: Tracker
 using Zygote: Zygote
 using Test
 
-Enzyme.API.runtimeActivity!(true)
+Random.seed!(0)
 
 test_differentiation(
     [
         AutoZygote(),
+        AutoTracker(),
         # AutoEnzyme()  # TODO: fix
     ],
     DIT.flux_scenarios();
