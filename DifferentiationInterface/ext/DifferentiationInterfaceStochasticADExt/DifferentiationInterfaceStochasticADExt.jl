0bf0c464079fbdf23345c2190c4ebae7f6e71f07
Arnau Quera-Bofaull <arnauq@protonmail.com>
2024-07-24 13:42:25 +0100

    only pushforward for now

--- DifferentiationInterface/ext/DifferentiationInterfaceStochasticADExt/DifferentiationInterfaceStochasticADExt.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceStochasticADExt/DifferentiationInterfaceStochasticADExt.jl
@@ -7,10 +7,8 @@ using StochasticAD: derivative_estimate
 
 DI.check_available(::AutoStochasticAD) = true
 DI.twoarg_support(::AutoStochasticAD) = DI.TwoArgNotSupported()
-DI.pullback_performance(::AutoStochasticAD) = DI.PushforwardSlow()
 
 DI.prepare_pushforward(f, ::AutoStochasticAD, x, dx) = NoPushforwardExtras()
-DI.prepare_pullback(f, ::AutoStochasticAD, x, dx) = NoPullbackExtras()
 
 function DI.pushforward(
     f, ad::AutoStochasticAD, x, dx, ::NoPushforwardExtras)
@@ -21,19 +19,6 @@ function DI.value_and_pushforward(
     f, backend::AutoStochasticAD, x, dx, extras::NoPushforwardExtras
 )
     return f(x), DI.pushforward(f, backend, x, dx, extras)
-end
-
-function DI.pullback(
-    f, backend::AutoStochasticAD, x, dy, extras::NoPullbackExtras
-)
-    jacobian = reduce(hcat, sum(derivative_estimate(f, x) for _ in 1:backend.n_samples) / backend.n_samples)
-    return jacobian' * dy
-end
-
-function DI.value_and_pullback(
-    f, backend::AutoStochasticAD, x, dy, extras::NoPullbackExtras
-)
-    return f(x), DI.pullback(f, backend, x, dy, extras)
 end
 
 end # module
