f55996cfca9a84965efb41344532bcad53c902e1
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-08-02 19:16:52 +0200

    Fully remove `constant_function` for `AutoEnzyme` following ADTypes yanking

--- DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/utils.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/utils.jl
@@ -1,11 +1,11 @@
-struct AutoDeferredEnzyme{M,constant_function} <: ADTypes.AbstractADType
+struct AutoDeferredEnzyme{M} <: ADTypes.AbstractADType
     mode::M
 end
 
 ADTypes.mode(backend::AutoDeferredEnzyme) = ADTypes.mode(AutoEnzyme(backend.mode))
 
-function DI.nested(backend::AutoEnzyme{M,constant_function}) where {M,constant_function}
-    return AutoDeferredEnzyme{M,constant_function}(backend.mode)
+function DI.nested(backend::AutoEnzyme{M}) where {M}
+    return AutoDeferredEnzyme{M}(backend.mode)
 end
 
 const AnyAutoEnzyme{M,constant_function} = Union{
@@ -33,20 +33,3 @@ function DI.basis(::AnyAutoEnzyme, a::AbstractArray{T}, i::CartesianIndex) where
 end
 
 get_f_and_df(f, ::AnyAutoEnzyme) = Const(f)
-
-#=
-# commented out until Enzyme errors when non-duplicated data is written to
-
-function get_f_and_df(f, backend::AnyAutoEnzyme{M,false}) where {M}
-    mode = isnothing(backend.mode) ? Reverse : backend.mode
-    A = guess_activity(typeof(f), mode)
-    if A <: Const || A <: Active
-        return Const(f)
-    elseif A <: Duplicated || A <: DuplicatedNoNeed || A <: MixedDuplicated
-        df = make_zero(f)
-        return Duplicated(f, df)
-    else
-        error("Unexpected activity guessed for the function `f`.")
-    end
-end
-=#
