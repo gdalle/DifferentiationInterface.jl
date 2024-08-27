5df4635ea9d5dd100bc92166e2e9d2bed764a3e7
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-08-27 20:30:23 +0200

    Debug Enzyme

--- DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/reverse_onearg.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/reverse_onearg.jl
@@ -24,75 +24,63 @@ end
 
 function DI.value_and_pullback(
     f,
-    backend::AnyAutoEnzyme{<:Union{ReverseMode,Nothing}},
-    x::Number,
-    ty::Tangents{1,<:Number},
-    ::NoPullbackExtras,
-)
-    dy = only(ty)
-    f_and_df = get_f_and_df(f, backend)
-    der, y = if backend isa AutoDeferredEnzyme
-        autodiff_deferred(ReverseWithPrimal, f_and_df, Active, Active(x))
-    else
-        autodiff(ReverseWithPrimal, f_and_df, Active, Active(x))
-    end
-    new_dx = dy * only(der)
-    return y, SingleTangent(new_dx)
-end
-
-function DI.value_and_pullback(
-    f,
     backend::AnyAutoEnzyme{<:Union{ReverseMode,Nothing},function_annotation},
     x::Number,
     ty::Tangents{1},
     ::NoPullbackExtras,
 ) where {function_annotation}
-    dy = only(ty)
-    f_and_df = force_annotation(get_f_and_df(f, backend))
-    mode = if function_annotation <: Annotation
-        ReverseSplitWithPrimal
+    if eltype(ty) <: Number
+        dy = only(ty)
+        f_and_df = get_f_and_df(f, backend)
+        der, y = if backend isa AutoDeferredEnzyme
+            autodiff_deferred(ReverseWithPrimal, f_and_df, Active, Active(x))
+        else
+            autodiff(ReverseWithPrimal, f_and_df, Active, Active(x))
+        end
+        new_dx = dy * only(der)
+        return y, SingleTangent(new_dx)
     else
-        my_set_err_if_func_written(ReverseSplitWithPrimal)
+        dy = only(ty)
+        f_and_df = force_annotation(get_f_and_df(f, backend))
+        mode = if function_annotation <: Annotation
+            ReverseSplitWithPrimal
+        else
+            my_set_err_if_func_written(ReverseSplitWithPrimal)
+        end
+        forw, rev = autodiff_thunk(mode, typeof(f_and_df), Duplicated, typeof(Active(x)))
+        tape, y, new_dy = forw(f_and_df, Active(x))
+        copyto!(new_dy, dy)
+        new_dx = only(only(rev(f_and_df, Active(x), tape)))
+        return y, SingleTangent(new_dx)
     end
-    forw, rev = autodiff_thunk(mode, typeof(f_and_df), Duplicated, typeof(Active(x)))
-    tape, y, new_dy = forw(f_and_df, Active(x))
-    copyto!(new_dy, dy)
-    new_dx = only(only(rev(f_and_df, Active(x), tape)))
-    return y, SingleTangent(new_dx)
 end
 
 function DI.value_and_pullback(
     f,
-    backend::AnyAutoEnzyme{<:Union{ReverseMode,Nothing}},
-    x,
-    ty::Tangents{1,<:Number},
-    ::NoPullbackExtras,
-)
-    dy = only(ty)
-    f_and_df = get_f_and_df(f, backend)
-    dx_sametype = make_zero(x)
-    x_and_dx = Duplicated(x, dx_sametype)
-    _, y = if backend isa AutoDeferredEnzyme
-        autodiff_deferred(ReverseWithPrimal, f_and_df, Active, x_and_dx)
-    else
-        autodiff(ReverseWithPrimal, f_and_df, Active, x_and_dx)
-    end
-    if !isone(dy)
-        # TODO: generalize beyond Arrays?
-        dx_sametype .*= dy
-    end
-    return y, SingleTangent(dx_sametype)
-end
-
-function DI.value_and_pullback(
-    f,
-    backend::AnyAutoEnzyme{<:Union{ReverseMode,Nothing}},
+    backend::AnyAutoEnzyme{<:Union{ReverseMode,Nothing},function_annotation},
     x,
     ty::Tangents{1},
-    extras::NoPullbackExtras,
-)
-    dx = make_zero(x)
-    return DI.value_and_pullback!(f, SingleTangent(dx), backend, x, ty, extras)
+    ::NoPullbackExtras,
+) where {function_annotation}
+    if eltype(ty) <: Number
+        dy = only(ty)
+        f_and_df = get_f_and_df(f, backend)
+        dx_sametype = make_zero(x)
+        x_and_dx = Duplicated(x, dx_sametype)
+        _, y = if backend isa AutoDeferredEnzyme
+            autodiff_deferred(ReverseWithPrimal, f_and_df, Active, x_and_dx)
+        else
+            autodiff(ReverseWithPrimal, f_and_df, Active, x_and_dx)
+        end
+        if !isone(dy)
+            # TODO: generalize beyond Arrays?
+            dx_sametype .*= dy
+        end
+        return y, SingleTangent(dx_sametype)
+    else
+        dx = make_zero(x)
+        return DI.value_and_pullback!(f, SingleTangent(dx), backend, x, ty, extras)
+    end
 end
 
 function DI.pullback(
@@ -110,53 +98,46 @@ end
 function DI.value_and_pullback!(
     f,
     tx::Tangents{1},
-    backend::AnyAutoEnzyme{<:Union{ReverseMode,Nothing}},
-    x,
-    ty::Tangents{1,<:Number},
-    ::NoPullbackExtras,
-)
-    dx, dy = only(tx), only(ty)
-    f_and_df = get_f_and_df(f, backend)
-    dx_sametype = convert(typeof(x), dx)
-    make_zero!(dx_sametype)
-    x_and_dx = Duplicated(x, dx_sametype)
-    _, y = if backend isa AutoDeferredEnzyme
-        autodiff_deferred(ReverseWithPrimal, f_and_df, Active, x_and_dx)
-    else
-        autodiff(ReverseWithPrimal, f_and_df, Active, x_and_dx)
-    end
-    if !isone(dy)
-        # TODO: generalize beyond Arrays?
-        dx_sametype .*= dy
-    end
-    copyto!(dx, dx_sametype)
-    return y, tx
-end
-
-function DI.value_and_pullback!(
-    f,
-    tx::Tangents{1},
     backend::AnyAutoEnzyme{<:Union{ReverseMode,Nothing},function_annotation},
     x,
     ty::Tangents{1},
     ::NoPullbackExtras,
 ) where {function_annotation}
-    dx, dy = only(tx), only(ty)
-    f_and_df = force_annotation(get_f_and_df(f, backend))
-    mode = if function_annotation <: Annotation
-        ReverseSplitWithPrimal
+    if eltype(ty) <: Number
+        dx, dy = only(tx), only(ty)
+        f_and_df = get_f_and_df(f, backend)
+        dx_sametype = convert(typeof(x), dx)
+        make_zero!(dx_sametype)
+        x_and_dx = Duplicated(x, dx_sametype)
+        _, y = if backend isa AutoDeferredEnzyme
+            autodiff_deferred(ReverseWithPrimal, f_and_df, Active, x_and_dx)
+        else
+            autodiff(ReverseWithPrimal, f_and_df, Active, x_and_dx)
+        end
+        if !isone(dy)
+            # TODO: generalize beyond Arrays?
+            dx_sametype .*= dy
+        end
+        copyto!(dx, dx_sametype)
+        return y, tx
     else
-        my_set_err_if_func_written(ReverseSplitWithPrimal)
+        dx, dy = only(tx), only(ty)
+        f_and_df = force_annotation(get_f_and_df(f, backend))
+        mode = if function_annotation <: Annotation
+            ReverseSplitWithPrimal
+        else
+            my_set_err_if_func_written(ReverseSplitWithPrimal)
+        end
+        dx_sametype = convert(typeof(x), dx)
+        make_zero!(dx_sametype)
+        x_and_dx = Duplicated(x, dx_sametype)
+        forw, rev = autodiff_thunk(mode, typeof(f_and_df), Duplicated, typeof(x_and_dx))
+        tape, y, new_dy = forw(f_and_df, x_and_dx)
+        copyto!(new_dy, dy)
+        rev(f_and_df, x_and_dx, tape)
+        copyto!(dx, dx_sametype)
+        return y, tx
     end
-    dx_sametype = convert(typeof(x), dx)
-    make_zero!(dx_sametype)
-    x_and_dx = Duplicated(x, dx_sametype)
-    forw, rev = autodiff_thunk(mode, typeof(f_and_df), Duplicated, typeof(x_and_dx))
-    tape, y, new_dy = forw(f_and_df, x_and_dx)
-    copyto!(new_dy, dy)
-    rev(f_and_df, x_and_dx, tape)
-    copyto!(dx, dx_sametype)
-    return y, tx
 end
 
 function DI.pullback!(
