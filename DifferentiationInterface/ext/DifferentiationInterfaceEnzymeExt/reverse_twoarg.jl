4489ad0c45b9892cfeaf697a07ffe01a39bed028
ExpandingMan <savastio@protonmail.com>
2024-09-18 19:18:34 -0400

    small fix

--- DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/reverse_twoarg.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/reverse_twoarg.jl
@@ -34,9 +34,9 @@ function DI.value_and_pullback(
     dy_sametype = convert(typeof(y), copy(dy))
     y_and_dy = Duplicated(y, dy_sametype)
     _, new_dx = if backend isa AutoDeferredEnzyme
-        only(autodiff_deferred(reverse_mode(backend), f!_and_df!, Const, y_and_dy, Active(x)))
+        only(autodiff_deferred(mode_noprimal(backend), f!_and_df!, Const, y_and_dy, Active(x)))
     else
-        only(autodiff(reverse_mode(backend), f!_and_df!, Const, y_and_dy, Active(x)))
+        only(autodiff(mode_noprimal(backend), f!_and_df!, Const, y_and_dy, Active(x)))
     end
     return y, Tangents(new_dx)
 end
@@ -56,9 +56,9 @@ function DI.value_and_pullback(
     y_and_dy = Duplicated(y, dy_sametype)
     x_and_dx = Duplicated(x, dx_sametype)
     if backend isa AutoDeferredEnzyme
-        autodiff_deferred(reverse_mode(backend), f!_and_df!, Const, y_and_dy, x_and_dx)
+        autodiff_deferred(mode_noprimal(backend), f!_and_df!, Const, y_and_dy, x_and_dx)
     else
-        autodiff(reverse_mode(backend), f!_and_df!, Const, y_and_dy, x_and_dx)
+        autodiff(mode_noprimal(backend), f!_and_df!, Const, y_and_dy, x_and_dx)
     end
     return y, Tangents(dx_sametype)
 end
