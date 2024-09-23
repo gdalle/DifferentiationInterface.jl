13690107794c94071ca7118a4fe185d5447eaf4e
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-09-23 16:32:13 +0200

    Make `DifferentiateWith` compatible with ForwardDiff, clarify docs (#487)
    * Make DIfferentiateWith compatible with ForwardDiff, clarify docs
    
    * More details

--- DifferentiationInterface/src/misc/differentiate_with.jl
+++ DifferentiationInterface/src/misc/differentiate_with.jl
@@ -1,18 +1,28 @@
 """
     DifferentiateWith
 
-Callable function wrapper that enforces differentiation with a specified (inner) backend.
+Function wrapper that enforces differentiation with a "substitute" AD backend, possible different from the "true" AD backend that is called.
 
-This works by defining new rules overriding the behavior of the outer backend that would normally be used.
+For instance, suppose a function `f` is not differentiable with Zygote because it involves mutation, but you know that it is differentiable with Enzyme.
+Then `f2 = DifferentiateWith(f, AutoEnzyme())` is a new function that behaves like `f`, except that `f2` is differentiable with Zygote (thanks to a chain rule which calls Enzyme under the hood).
+Moreover, any larger algorithm `alg` that calls `f2` instead of `f` will also be differentiable with Zygote (as long as `f` was the only Zygote blocker).
+
+!!! tip
+    This is mainly relevant for package developers who want to produce differentiable code at low cost, without writing the differentiation rules themselves.
+    If you sprinkle a few `DifferentiateWith` in places where some AD backends may struggle, end users can pick from a wider variety of packages to differentiate your algorithms.
 
 !!! warning
-    This is an experimental functionality, whose API cannot yet be considered stable.
-    It only supports out-of-place functions, and rules are only defined for [ChainRules.jl](https://github.com/JuliaDiff/ChainRules.jl)-compatible outer backends.
+    `DifferentiateWith` only supports out-of-place functions `y = f(x)` without additional context arguments.
+    It only makes these functions differentiable if the true backend is either [ForwardDiff](https://github.com/JuliaDiff/ForwardDiff.jl) or compatible with [ChainRules](https://github.com/JuliaDiff/ChainRules.jl).
+    For any other true backend, the differentiation behavior is not altered by `DifferentiateWith` (it becomes a transparent wrapper).
 
 # Fields
 
-- `f`: the function in question
-- `backend::AbstractADType`: the inner backend to use for differentiation
+- `f`: the function in question, with signature `f(x)`
+- `backend::AbstractADType`: the substitute backend to use for differentiation
+
+!!! note
+    For the substitute AD backend to be called under the hood, its package needs to be loaded in addition to the package of the true AD backend.
 
 # Constructor
 
@@ -21,23 +31,32 @@ This works by defining new rules overriding the behavior of the outer backend th
 # Example
 
 ```jldoctest
-using DifferentiationInterface
-import ForwardDiff, Zygote
+julia> using DifferentiationInterface
 
-function f(x)
-    a = Vector{eltype(x)}(undef, 1)
-    a[1] = sum(x)  # mutation that breaks Zygote
-    return a[1]
-end
+julia> import Enzyme, ForwardDiff, Zygote
 
-dw = DifferentiateWith(f, AutoForwardDiff());
+julia> function f(x::Vector{Float64})
+           a = Vector{Float64}(undef, 1)  # type constraint breaks ForwardDiff
+           a[1] = sum(abs2, x)  # mutation breaks Zygote
+           return a[1]
+       end;
 
-gradient(dw, AutoZygote(), [2.0])  # calls ForwardDiff instead
+julia> f2 = DifferentiateWith(f, AutoEnzyme());
 
-# output
+julia> f([3.0, 5.0]) == f2([3.0, 5.0])
+true
 
-1-element Vector{Float64}:
- 1.0
+julia> alg(x) = 7 * f2(x);
+
+julia> ForwardDiff.gradient(alg, [3.0, 5.0])
+2-element Vector{Float64}:
+ 42.0
+ 70.0
+
+julia> Zygote.gradient(alg, [3.0, 5.0])[1]
+2-element Vector{Float64}:
+ 42.0
+ 70.0
 ```
 """
 struct DifferentiateWith{F,B<:AbstractADType}
@@ -45,16 +64,17 @@ struct DifferentiateWith{F,B<:AbstractADType}
     backend::B
 end
 
-"""
-    (dw::DifferentiateWith)(x)
-
-Call the underlying function `dw.f` of a [`DifferentiateWith`](@ref) wrapper.
-"""
 (dw::DifferentiateWith)(x) = dw.f(x)
 
 function Base.show(io::IO, dw::DifferentiateWith)
     @compat (; f, backend) = dw
     return print(
-        io, DifferentiateWith, "(", repr(f; context=io), ",", repr(backend; context=io), ")"
+        io,
+        DifferentiateWith,
+        "(",
+        repr(f; context=io),
+        ", ",
+        repr(backend; context=io),
+        ")",
     )
 end
