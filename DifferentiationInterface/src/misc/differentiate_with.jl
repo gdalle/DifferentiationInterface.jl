604e06768910464e4c1c80fecf21f7380d17bf9e
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-09-23 15:50:44 +0200

    More details

--- DifferentiationInterface/src/misc/differentiate_with.jl
+++ DifferentiationInterface/src/misc/differentiate_with.jl
@@ -1,15 +1,20 @@
 """
     DifferentiateWith
 
-Function wrapper that enforces differentiation with a substitute AD backend, different from the true AD backend that the user intends to call.
+Function wrapper that enforces differentiation with a "substitute" AD backend, possible different from the "true" AD backend that is called.
 
-For instance, suppose `f` is not differentiable with Zygote because it involves mutation, but you know that it is differentiable with Enzyme.
-Then `g = DifferentiateWith(f, AutoEnzyme())` is differentiable with Zygote thanks to a chain rule which calls Enzyme under the hood.
-Moreover, any composition involving `g` will also be differentiable.
+For instance, suppose a function `f` is not differentiable with Zygote because it involves mutation, but you know that it is differentiable with Enzyme.
+Then `f2 = DifferentiateWith(f, AutoEnzyme())` is a new function that behaves like `f`, except that `f2` is differentiable with Zygote (thanks to a chain rule which calls Enzyme under the hood).
+Moreover, any larger algorithm `alg` that calls `f2` instead of `f` will also be differentiable with Zygote (as long as `f` was the only Zygote blocker).
+
+!!! tip
+    This is mainly relevant for package developers who want to produce differentiable code at low cost, without writing the differentiation rules themselves.
+    If you sprinkle a few `DifferentiateWith` in places where some AD backends may struggle, end users can pick from a wider variety of packages to differentiate your algorithms.
 
 !!! warning
     `DifferentiateWith` only supports out-of-place functions `y = f(x)` without additional context arguments.
-    It only makes these functions differentiable if the true backend is either [ForwardDiff](https://github.com/JuliaDiff/ForwardDiff.jl) or anything [ChainRules](https://github.com/JuliaDiff/ChainRules.jl)-compatible.
+    It only makes these functions differentiable if the true backend is either [ForwardDiff](https://github.com/JuliaDiff/ForwardDiff.jl) or compatible with [ChainRules](https://github.com/JuliaDiff/ChainRules.jl).
+    For any other true backend, the differentiation behavior is not altered by `DifferentiateWith` (it becomes a transparent wrapper).
 
 # Fields
 
@@ -36,16 +41,19 @@ julia> function f(x::Vector{Float64})
            return a[1]
        end;
 
-julia> g = DifferentiateWith(f, AutoEnzyme());
+julia> f2 = DifferentiateWith(f, AutoEnzyme());
 
-julia> h(x) = 7 * g(x);
+julia> f([3.0, 5.0]) == f2([3.0, 5.0])
+true
 
-julia> ForwardDiff.gradient(h, [3.0, 5.0])
+julia> alg(x) = 7 * f2(x);
+
+julia> ForwardDiff.gradient(alg, [3.0, 5.0])
 2-element Vector{Float64}:
  42.0
  70.0
 
-julia> Zygote.gradient(h, [3.0, 5.0])[1]
+julia> Zygote.gradient(alg, [3.0, 5.0])[1]
 2-element Vector{Float64}:
  42.0
  70.0
