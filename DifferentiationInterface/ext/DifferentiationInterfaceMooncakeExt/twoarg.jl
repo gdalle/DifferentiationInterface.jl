fdebc3ecdcad24ef4b0a15a685a7ce8791fdc18c
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-09-30 11:22:52 +0200

    Improve Mooncake caching

--- DifferentiationInterface/ext/DifferentiationInterfaceMooncakeExt/twoarg.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceMooncakeExt/twoarg.jl
@@ -1,5 +1,10 @@
-struct MooncakeTwoArgPullbackPrep{R} <: PullbackPrep
+struct MooncakeTwoArgPullbackPrep{R,F,Y,DX,DY} <: PullbackPrep
     rrule::R
+    df!::F
+    y_copy::Y
+    dx_righttype::DX
+    dy_righttype::DY
+    dy_righttype_after::DY
 end
 
 function DI.prepare_pullback(
@@ -12,7 +17,14 @@ function DI.prepare_pullback(
         debug_mode=config.debug_mode,
         silence_debug_messages=config.silence_debug_messages,
     )
-    prep = MooncakeTwoArgPullbackPrep(rrule)
+    df! = zero_tangent(f!)
+    y_copy = copy(y)
+    dx_righttype = zero_tangent(x)
+    dy_righttype = zero_tangent(y)
+    dy_righttype_after = zero_tangent(y)
+    prep = MooncakeTwoArgPullbackPrep(
+        rrule, df!, y_copy, dx_righttype, dy_righttype, dy_righttype_after
+    )
     DI.value_and_pullback(f!, y, prep, backend, x, ty, contexts...)  # warm up
     return prep
 end
@@ -27,29 +39,22 @@ function DI.value_and_pullback(
     contexts::Vararg{Context,C},
 ) where {C}
     dy = only(ty)
-    dy_righttype = convert(tangent_type(typeof(y)), copy(dy))
+
+    # Set all tangent storage to zero.
+    df! = set_to_zero!!(prep.df!)
+    # dx_righttype = set_to_zero!!(prep.dx_righttype)  # TODO: why doesn't this work?
     dx_righttype = zero_tangent(x)
+    dy_righttype = set_to_zero!!(prep.dy_righttype)
 
-    # We want the VJP, not VJP + dx, so I'm going to zero-out `dx`. `set_to_zero!!` has the advantage
-    # that it will also replace any immutable components of `dx` to zero.
-    dx_righttype = set_to_zero!!(dx_righttype)
+    # Prepare cotangent to add after the forward pass.
+    dy_righttype_after = copyto!(prep.dy_righttype_after, dy)
 
-    # We want `dy` to correspond to the cotangent of `y` _after_
-    # running the forwards-pass, so I'm going to take a copy, and zero-out the original.
-    dy_righttype_backup = copy(dy_righttype)
-    dy_righttype = set_to_zero!!(dy_righttype)
     contexts_coduals = map(zero_fcodual ∘ unwrap, contexts)
 
-    # Mutate a copy of `y`, so that we can run the reverse-pass later on.
-    y_copy = copy(y)
-
-    # In case `f!` is a closure
-    df! = zero_tangent(f!)
-
-    # Run the forwards-pass.
+    # Run the forward pass
     out, pb!! = prep.rrule(
         CoDual(f!, fdata(df!)),
-        CoDual(y_copy, fdata(dy_righttype)),
+        CoDual(prep.y_copy, fdata(dy_righttype)),
         CoDual(x, fdata(dx_righttype)),
         contexts_coduals...,
     )
@@ -57,13 +62,13 @@ function DI.value_and_pullback(
     # Verify that the output is non-differentiable.
     @assert primal(out) === nothing
 
-    # Set the cotangent of `y` to be equal to the requested value.
-    dy_righttype = increment!!(dy_righttype, dy_righttype_backup)
+    # Increment the desired cotangent dy.
+    dy_righttype = increment!!(dy_righttype, dy_righttype_after)
 
-    # Record the state of `y` before running the reverse-pass.
-    y = copyto!(y, y_copy)
+    # Record the state of y before running the reverse pass.
+    y = copyto!(y, prep.y_copy)
 
-    # Run the reverse-pass.
+    # Run the reverse pass.
     _, _, new_dx = pb!!(NoRData())
 
     return y, (tangent(fdata(dx_righttype), new_dx),)
