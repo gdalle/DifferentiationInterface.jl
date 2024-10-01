a16f24d64606e6464702197be16c75b1c6cc2c02
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-10-01 08:16:52 +0200

    Group tests together

--- DifferentiationInterface/test/runtests.jl
+++ DifferentiationInterface/test/runtests.jl
@@ -14,17 +14,11 @@ GROUP = get(ENV, "JULIA_DI_TEST_GROUP", "All")
 ## Main tests
 
 @testset verbose = true "DifferentiationInterface.jl" begin
-    for category in readdir(@__DIR__)
-        isdir(category) || continue
-        for folder in readdir(joinpath(@__DIR__, category))
-            if GROUP == "All" || (category, folder) == split(GROUP, '/')
-                @testset verbose = true "$category/$folder" begin
-                    @testset "$file" for file in
-                                         readdir(joinpath(@__DIR__, category, folder))
-                        @info "Testing $category/$folder/$file"
-                        include(joinpath(@__DIR__, category, folder, file))
-                    end
-                end
+    for (category, folder) in split.(GROUP, '/')
+        @testset verbose = true "$category/$folder" begin
+            @testset "$file" for file in readdir(joinpath(@__DIR__, category, folder))
+                @info "Testing $category/$folder/$file"
+                include(joinpath(@__DIR__, category, folder, file))
             end
         end
     end
