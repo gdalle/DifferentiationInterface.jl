05cddc4dee3c3b11b7b1f67d71a631e216154867
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-10-14 17:30:01 +0200

    Fix

--- DifferentiationInterface/test/Misc/FromPrimitive/test.jl
+++ DifferentiationInterface/test/Misc/FromPrimitive/test.jl
@@ -65,13 +65,17 @@ test_differentiation(
 
 ## Misc
 
-jac_for_prep = prepare_jacobian(copy, MyAutoSparse(backends[1]), rand(10));
-jac_rev_prep = prepare_jacobian(copy, MyAutoSparse(backends[2]), rand(10));
-hess_prep = prepare_hessian(x -> sum(abs2, x), MyAutoSparse(backends[1]), rand(10));
+@testset "SparseMatrixColorings access" begin
+    jac_for_prep = prepare_jacobian(copy, MyAutoSparse(adaptive_backends[1]), rand(10))
+    jac_rev_prep = prepare_jacobian(copy, MyAutoSparse(adaptive_backends[2]), rand(10))
+    hess_prep = prepare_hessian(
+        x -> sum(abs2, x), MyAutoSparse(adaptive_backends[1]), rand(10)
+    )
 
-@test all(==(1), column_colors(jac_for_prep))
-@test all(==(1), row_colors(jac_rev_prep))
-@test all(==(1), column_colors(hess_prep))
-@test only(column_groups(jac_for_prep)) == 1:10
-@test only(row_groups(jac_rev_prep)) == 1:10
-@test only(column_groups(hess_prep)) == 1:10
+    @test all(==(1), column_colors(jac_for_prep))
+    @test all(==(1), row_colors(jac_rev_prep))
+    @test all(==(1), column_colors(hess_prep))
+    @test only(column_groups(jac_for_prep)) == 1:10
+    @test only(row_groups(jac_rev_prep)) == 1:10
+    @test only(column_groups(hess_prep)) == 1:10
+end
