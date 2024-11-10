ceeb19c5ace1b01097ed02548a9251b72e9003e6
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-11-10 08:35:40 +0100

    Add allocation testing

--- DifferentiationInterface/test/Back/ForwardDiff/test.jl
+++ DifferentiationInterface/test/Back/ForwardDiff/test.jl
@@ -3,6 +3,7 @@ Pkg.add("ForwardDiff")
 
 using ComponentArrays: ComponentArrays
 using DifferentiationInterface, DifferentiationInterfaceTest
+import DifferentiationInterfaceTest as DIT
 using ForwardDiff: ForwardDiff
 using StaticArrays: StaticArrays
 using Test
@@ -65,3 +66,19 @@ test_differentiation(
 ## Static
 
 test_differentiation(AutoForwardDiff(), static_scenarios(); logging=LOGGING)
+
+@testset verbose = true "No allocations on StaticArrays" begin
+    filtered_static_scenarios = filter(static_scenarios(; include_batchified=false)) do scen
+        DIT.function_place(scen) == :out && DIT.operator_place(scen) == :out
+    end
+    data = benchmark_differentiation(
+        AutoForwardDiff(),
+        filtered_static_scenarios;
+        benchmark=:prepared,
+        excluded=[:hessian, :pullback],  # TODO: figure this out
+        logging=LOGGING,
+    )
+    @testset "$(row[:scenario])" for row in eachrow(data)
+        @test row[:allocs] == 0
+    end
+end;
