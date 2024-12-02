c60321406d88ff5b68a7db54d4afb1707d9ba4c1
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-12-02 16:04:09 +0100

    Public tests on 1.11

--- DifferentiationInterface/test/Misc/Internals/_formalities.jl
+++ DifferentiationInterface/test/Misc/Internals/_formalities.jl
@@ -33,8 +33,11 @@ end
     @test check_no_implicit_imports(DifferentiationInterface) === nothing
     @test check_no_stale_explicit_imports(DifferentiationInterface) === nothing
     @test check_all_explicit_imports_via_owners(DifferentiationInterface) === nothing
-    @test check_all_explicit_imports_are_public(DifferentiationInterface) === nothing
     @test check_all_qualified_accesses_via_owners(DifferentiationInterface) === nothing
-    @test_skip check_all_qualified_accesses_are_public(DifferentiationInterface) === nothing
     @test check_no_self_qualified_accesses(DifferentiationInterface) === nothing
+    if VERSION >= v"1.11"
+        @test check_all_explicit_imports_are_public(DifferentiationInterface) === nothing
+        @test_skip check_all_qualified_accesses_are_public(DifferentiationInterface) ===
+            nothing
+    end
 end
--- DifferentiationInterfaceTest/test/formalities.jl
+++ DifferentiationInterfaceTest/test/formalities.jl
@@ -32,10 +32,13 @@ end
     @test_broken check_no_stale_explicit_imports(DifferentiationInterfaceTest) === nothing
     @test_broken check_all_explicit_imports_via_owners(DifferentiationInterfaceTest) ===
         nothing
-    @test_broken check_all_explicit_imports_are_public(DifferentiationInterfaceTest) ===
-        nothing
     @test check_all_qualified_accesses_via_owners(DifferentiationInterfaceTest) === nothing
-    @test_broken check_all_qualified_accesses_are_public(DifferentiationInterfaceTest) ===
-        nothing
     @test check_no_self_qualified_accesses(DifferentiationInterfaceTest) === nothing
+    if VERSION >= v"1.11"
+        @test_broken check_all_explicit_imports_are_public(DifferentiationInterfaceTest) ===
+            nothing
+        @test_broken check_all_qualified_accesses_are_public(
+            DifferentiationInterfaceTest
+        ) === nothing
+    end
 end
